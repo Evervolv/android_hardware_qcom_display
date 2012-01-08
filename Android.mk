bd4bfd035d43882d1df45ed531cad9acc65c552e
andrew sutherland <drew@master.localdomain>
2012-01-07 22:40:24 -0600

    gralloc: add BOARD_USES_LEGACY_QCOM
    Change-Id: I7fae5cf67ed4a1f00012c64c384bb621ab8bb183

--- Android.mk
+++ Android.mk
@@ -1,7 +1,11 @@
 #Enables the listed display HAL modules
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+ifeq ($(BOARD_USES_LEGACY_QCOM),true)
+    display-hals := libgralloc-legacy libcopybit
+else
 	display-hals := libhwcomposer liboverlay libgralloc libcopybit libgenlock libtilerenderer
 	display-hals += libqcomui
+endif
 	include $(call all-named-subdir-makefiles,$(display-hals))
 endif
