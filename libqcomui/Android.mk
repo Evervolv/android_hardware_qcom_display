ab071af1143e8ad1ef5f462a7120ba1c58517b21
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-03-27 01:28:13 -0500

    libqcomui: qsd8k: enable BYPASS_EGLIMAGE CHECK_FOR_EXTERNAL_FORMAT
    these work arounds are originally for msm7x27a but seem to work ok on qsd8k
    
    Change-Id: Ie903598ff0d532d5a6d8ee4725977117ec1bfb36

--- libqcomui/Android.mk
+++ libqcomui/Android.mk
@@ -4,10 +4,10 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
         qcom_ui.cpp
 
-#ifeq ($(TARGET_BOARD_PLATFORM),qsd8k) # these are originally for 7x27a
-#    LOCAL_CFLAGS += -DCHECK_FOR_EXTERNAL_FORMAT
-#    LOCAL_CFLAGS += -DBYPASS_EGLIMAGE
-#endif
+ifeq ($(TARGET_BOARD_PLATFORM),qsd8k) # these are originally for 7x27a
+    LOCAL_CFLAGS += -DCHECK_FOR_EXTERNAL_FORMAT
+    LOCAL_CFLAGS += -DBYPASS_EGLIMAGE
+endif
 
 LOCAL_SHARED_LIBRARIES := \
         libutils \
