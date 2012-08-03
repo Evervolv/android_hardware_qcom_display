2d0ecf80e6131b1e5799bd13d01c6c699c5db4b4
Arun Kumar K.R <akumarkr@codeaurora.org>
2012-08-02 19:52:20 -0700

    libgralloc: include kernel headers
    Change-Id: If0a4983b7344e1ca58b316e0cb0cc964d1235946

--- libgralloc/Android.mk
+++ libgralloc/Android.mk
@@ -20,7 +20,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE                  := gralloc.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_PATH             := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS             := optional
-LOCAL_C_INCLUDES              := $(common_includes)
+LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libmemalloc libgenlock
 LOCAL_SHARED_LIBRARIES        += libqdutils libGLESv1_CM
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"gralloc\"
