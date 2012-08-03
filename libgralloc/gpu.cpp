36de7a412e1787f7a8e941a24bccbc81c82157d6
Ramkumar Radhakrishnan <ramkumar@codeaurora.org>
2012-08-03 12:53:55 -0700

    libgralloc: Add support to custom buffer sizes.
    Init allocSize hook function with alloc_size function
    
    CRs-fixed: 379299
    Change-Id: I514d47928f10b6c41404aa19d8b704f64327211b
    
    Conflicts:
    	libgralloc/gpu.cpp

--- libgralloc/gpu.cpp
+++ libgralloc/gpu.cpp
@@ -43,6 +43,7 @@ gpu_context_t::gpu_context_t(const private_module_t* module,
     common.module  = const_cast<hw_module_t*>(&module->base.common);
     common.close   = gralloc_close;
     alloc          = gralloc_alloc;
+    allocSize      = gralloc_alloc_size;
     free           = gralloc_free;
 
 }
