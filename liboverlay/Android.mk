cdbc18bae442354a159053c533bd9c061a9ac0ce
Steve Kondik <shade@chemlab.org>
2012-10-31 16:25:53 -0700

    display: Link liboverlay with libqdutils
    Change-Id: I81db07e5a3a3d66b11e18ecfce44b2bfd7d1f514

--- liboverlay/Android.mk
+++ liboverlay/Android.mk
@@ -4,8 +4,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE                  := liboverlay
 LOCAL_MODULE_PATH             := $(TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_MODULE_TAGS             := optional
-LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
-LOCAL_SHARED_LIBRARIES        := $(common_libs) libmemalloc
+LOCAL_C_INCLUDES              := $(common_includes)
+LOCAL_SHARED_LIBRARIES        := $(common_libs) libmemalloc libqdutils
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"overlay\"
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 LOCAL_SRC_FILES := \
