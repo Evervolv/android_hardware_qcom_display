183dd454615e9e98958a662830b1c7f7c3bd11b8
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-07-27 22:37:53 -0500

    utilize TARGET_USES_QCOM_BSP
    * mako doesnt want this
    
    Change-Id: If2a15fb629f5559fd74a6e52ada7d1a84e38155c

--- msm8960/common.mk
+++ msm8960/common.mk
@@ -29,8 +29,10 @@ common_deps  :=
 kernel_includes :=
 
 # Executed only on QCOM BSPs
+ifeq ($(TARGET_USES_QCOM_BSP),true)
+     common_flags += -DQCOM_BSP
+endif
 ifeq ($(call is-vendor-board-platform,QCOM),true)
-    common_flags += -DQCOM_BSP
     common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
     kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 endif
